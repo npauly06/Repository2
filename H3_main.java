47a97defbaa4422d162ac480e1c4a8230289e11d
npauly06 <npauly@smail.uni-koeln.de>
2025-10-31 01:06:19 +0100

    Hausaufgabe3npauly

---
+++ H3_main.java
@@ -0,0 +1,24 @@
+package h3;
+
+public class H3_main {
+
+	public static void main(String[] args) {
+		int i = 140;
+		int j = 150;
+		int k = -10;
+		
+		if (i>j && i<=200 && j<=100) {
+			k= 1;
+		} else if (i>j && i>200 && j<=100) {
+			k=2;
+		} else if (i>j && i>200 && j>100) {
+			k=3;
+		} else if (i<=j && i<=200 && j<=100) {
+			k=4;	
+		} else {
+			k=k;
+		}
+		
+		System.out.println("Wert von k: " + k);	
+	}
+}
